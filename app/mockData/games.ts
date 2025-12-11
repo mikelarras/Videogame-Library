c6ed399278711c59e30003ff23774a1dffaf26d2
Mikel Arrastia <mikel.arrastia@jakala.com>
2025-12-11 10:34:05 +0100

    chore: create some mocked games

---
+++ app/mockData/games.ts
@@ -0,0 +1,15 @@
+type ListItem = {
+    id: number;
+    name: string;
+    image: string;
+}
+
+export const games: ListItem[] = [
+    {id: 1, name: "Grand Theft Auto V", image: "./imagenes/gta-v.jpg"},
+    {id: 2, name: "Elden Ring", image: "./imagenes/elden-ring.jpg"},
+    {id: 3, name: "Cyberpunk 2077", image: "./imagenes/cyberpunk-2077.jpg"},
+    {id: 4, name: "Red Dead Redemption 2", image: "./imagenes/red-dead-redemption-2.jpg"},
+    {id: 5, name: "The Witcher 3: Wild Hunt", image: "./imagenes/the-witcher.jpg"},
+    {id: 6, name: "God of War", image: "./imagenes/god-of-war.jpg"},
+    {id: 7, name: "Detroit: Become Human", image: "./imagenes/detroit-become-human.jpg"},
+]

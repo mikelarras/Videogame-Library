e5ef952bc3c317d5037f48285a7fad8f20bf596e
Mikel Arrastia <mikel.arrastia@jakala.com>
2025-12-17 12:58:27 +0100

    fix: fix dependency injection error

--- app/components/GameList.tsx
+++ app/components/GameList.tsx
@@ -1,17 +1,29 @@
-import GameListCard from "./GameListCard";
-import { games } from "../mockData/games";
+import GameListCard from './GameListCard';
 import styles from './GameList.module.css';
+import { GameService } from '../core/services/gameService';
+import { GameRepository } from '../core/domain/GameRepository';
+import { Game } from '../core/domain/Game';
 
-export default function GameList() {
-  return (
-    <ol className={styles.gameList} data-testid="game-list">
-      {games.map((game) => (
-        <GameListCard
-          key={game.id}
-          gameImage={game.image}
-          gameName={game.name}
-        />
-      ))}
-    </ol>
-  );
+export default async function GameList({
+    gameRepository,
+}: {
+    gameRepository: GameRepository;
+}) {
+    const gameService = new GameService(gameRepository);
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const gameList = await gameService.getGamesByYear(currentYear);
+
+    console.log(gameList);
+    return (
+        <ol className={styles.gameList} data-testid="game-list">
+            {gameList.map((game: Game) => (
+                <GameListCard
+                    key={game.id}
+                    gameImage={game.cover}
+                    gameName={game.name}
+                />
+            ))}
+        </ol>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import Header from "./components/Header";
-import GameList from "./components/GameList";
+import Header from './components/Header';
+import GameList from './components/GameList';
 import styles from './page.module.css';
+import { ApiGameRepository } from './core/infraestructure/ApiGameRepository';
 
 export default function Home() {
-  return (
-    <div className={styles.content}>
-    <Header />
-    {/* <img src="./videogame-characters.png" alt="videogame" className={styles.welcomeImage}></img>
+    return (
+        <div className={styles.content}>
+            <Header />
+            {/* <img src="./videogame-characters.png" alt="videogame" className={styles.welcomeImage}></img>
     <h1 className={styles.welcomeText}>BIENVENIDO A TU BIBLIOTECA DE VIDEOJUEGOS</h1> */}
-    <GameList />
-    </div>
-  );
+            <GameList gameRepository={ApiGameRepository} />
+        </div>
+    );
 }

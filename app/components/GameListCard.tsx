4891c3e167097d00dd26f252d5fcd27bdf33e6f4
Mikel Arrastia <mikel.arrastia@jakala.com>
2025-12-17 13:00:27 +0100

    fix: check if cover image exists at insert in html tag

--- app/components/GameListCard.tsx
+++ app/components/GameListCard.tsx
@@ -1,11 +1,26 @@
 import styles from './GameListCard.module.css';
 
-export default function GameListCard({ gameImage, gameName }: { gameImage: string, gameName: string }) {
-  return (
-    <li className={styles.gameListCard} style={{ backgroundImage: `url(${gameImage})` }}>
-      <img className={styles.gameImage} src={gameImage} alt={gameName} />
-      <p>{gameName}</p>
-      {/* <img className={styles.plusIcon} src="./imagenes/plus-icon.svg" alt="añadir a la biblioteca" /> */}
-    </li>
-  );
+export default function GameListCard({
+    gameImage,
+    gameName,
+}: {
+    gameImage: string | null;
+    gameName: string;
+}) {
+    return (
+        <li
+            className={styles.gameListCard}
+            style={
+                gameImage ? { backgroundImage: `url(${gameImage})` } : undefined
+            }
+        >
+            <img
+                className={styles.gameImage}
+                src={gameImage ?? undefined}
+                alt={gameName}
+            />
+            <p>{gameName}</p>
+            {/* <img className={styles.plusIcon} src="./imagenes/plus-icon.svg" alt="añadir a la biblioteca" /> */}
+        </li>
+    );
 }

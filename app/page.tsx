58922c47712ffb9e9ccc583fadde739476c54973
Mikel Arrastia <mikel.arrastia@jakala.com>
2025-12-17 13:29:46 +0100

    chore: delete dependency injection from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import Header from './components/Header';
 import GameList from './components/GameList';
 import styles from './page.module.css';
-import { ApiGameRepository } from './core/infraestructure/ApiGameRepository';
 
 export default function Home() {
     return (
         <div className={styles.content}>
             <Header />
-            {/* <img src="./videogame-characters.png" alt="videogame" className={styles.welcomeImage}></img>
-    <h1 className={styles.welcomeText}>BIENVENIDO A TU BIBLIOTECA DE VIDEOJUEGOS</h1> */}
-            <GameList gameRepository={ApiGameRepository} />
+            <GameList />
         </div>
     );
 }

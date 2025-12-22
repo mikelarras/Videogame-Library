56f9bb6fe140163378d5d3cd6fa231921eeeaccd
Mikel Arrastia <mikel.arrastia@jakala.com>
2025-12-22 11:46:43 +0100

    feature: blur background image

--- app/components/GameListCard.tsx
+++ app/components/GameListCard.tsx
@@ -8,19 +8,27 @@ export default function GameListCard({
     gameName: string;
 }) {
     return (
-        <li
-            className={styles.gameListCard}
-            style={
-                gameImage ? { backgroundImage: `url(${gameImage})` } : undefined
-            }
-        >
+        <li className={styles.gameListCard}>
+            <div
+                className={styles.backgroundImage}
+                style={
+                    gameImage
+                        ? { backgroundImage: `url(${gameImage})` }
+                        : undefined
+                }
+            ></div>
+            <div className={styles.backgroundGradient}></div>
             <img
                 className={styles.gameImage}
                 src={gameImage ?? undefined}
-                alt={gameName}
+                alt={`Portada de ${gameName}`}
             />
             <p>{gameName}</p>
-            {/* <img className={styles.plusIcon} src="./imagenes/plus-icon.svg" alt="añadir a la biblioteca" /> */}
+            <img
+                className={styles.plusIcon}
+                src="./imagenes/plus-icon.svg"
+                alt="añadir a la biblioteca"
+            />
         </li>
     );
 }
